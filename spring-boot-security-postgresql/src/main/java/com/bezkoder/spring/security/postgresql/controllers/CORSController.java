73c0526f3cbdd1140312d4562352bdb57bb45c9c
fabricio hein <fabricio@exago.com>
2025-01-05 18:24:49 -0300

    cors

--- spring-boot-security-postgresql/src/main/java/com/bezkoder/spring/security/postgresql/controllers/CORSController.java
+++ spring-boot-security-postgresql/src/main/java/com/bezkoder/spring/security/postgresql/controllers/CORSController.java
@@ -15,7 +15,7 @@ public class CORSController {
                 .header("Access-Control-Allow-Origin", "*")
                 .header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
                 .header("Access-Control-Allow-Headers", "Authorization, Content-Type")
-                .header("Access-Control-Allow-Credentials", "true")
+                .header("Access-Control-Allow-Credentials", "false")
                 .build();
     }
 }
