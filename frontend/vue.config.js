89b79803c03b1ff53054e08ec9d4eac3572ac153
fabricio hein <fabricio@exago.com>
2025-01-05 15:47:58 -0300

    fix

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,65 +1,58 @@
 const path = require('path');
 
+// Verifique o ambiente no qual o Node.js está executando
 console.log(process.env.NODE_ENV);
 
 module.exports = {
+  publicPath: '/',
+  outputDir: 'dist',
+  lintOnSave: true,
+  runtimeCompiler: false,
+  productionSourceMap: false,
 
-    publicPath: '/',
-    outputDir: 'dist',
-    lintOnSave: true,
-    runtimeCompiler: false,
-    productionSourceMap: false,
-    devServer: {
-      port: 8080,
-      https: false,
-      hotOnly: false,
-      proxy: {
-        '/api': {
-            target: 'https://mural-app.onrender.com',
-            // target: 'https://us-central1-linkkoub-prod.cloudfunctions.net/backend',
-            changeOrigin: true,
-            rewrite: (path) => path.replace(/^\/api/, ''),
-            secure: true,
+  devServer: {
+    port: 8080,
+    https: false,
+    hotOnly: false,
+    proxy: {
+      '/api': {
+        target: process.env.BACKEND,  // Usando variável de ambiente para o target
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        secure: true,
+      }
+      
+    },
+    disableHostCheck: true, // Desabilita a verificação de host
+  },
 
-        }         
+  configureWebpack: {
+    devtool: 'source-map',
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'), // Usando alias corretamente para simplificar os caminhos de importação
+      },
+      fallback: {},
     },
-      disableHostCheck: true,
-    },
-    
+  },
 
-    configureWebpack: {
-        devtool: 'source-map'
-    },
-    chainWebpack: config => {
-        config.plugins.delete('prefetch');
-    },
-    pluginOptions: {
-        'style-resources-loader': {
-            preProcessor: 'scss',
-            patterns: [
-                       path.resolve(__dirname, 'src/assets/sass/app.scss')
-                // Adicione caminhos para seus arquivos SCSS globais, se necessário
-            ]
-        },
-        i18n: {
-            locale: 'en',
-            fallbackLocale: 'en',
-            localeDir: 'locales',
-            enableInSFC: false,
-        },
-    },
-    chainWebpack: (config) => {
-        // Remove prefetch plugin and that's it!
-        config.plugins.delete('prefetch');
-    },
-    configureWebpack: {
-        resolve: {
-            alias: {
-                '@themeConfig': path.resolve(__dirname, 'theme.config.js'),
-            },
-            fallback: {
+  chainWebpack: (config) => {
+    // Remove o plugin 'prefetch', que pode não ser necessário para seu caso
+    config.plugins.delete('prefetch');
+  },
 
-            },
-        },
-    }
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'scss',
+      patterns: [
+        path.resolve(__dirname, 'src/assets/sass/app.scss'), // Caminho para o SCSS global
+      ],
+    },
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: false,
+    },
+  },
 };
